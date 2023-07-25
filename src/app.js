0e7773ae44c82c4af15d0bacde0f29ed901146be
Levy <levymcruz@gmail.com>
2023-07-25 20:19:32 -0300

    att

--- src/app.js
+++ src/app.js
@@ -17,6 +17,6 @@ app.use(router);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`
-    Running API AutoGoGo on port ${PORT},
+    API Projeto 16 na porta ${PORT},
     Url: http://localhost:${PORT} 
 `));
