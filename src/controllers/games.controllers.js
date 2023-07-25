ba4548c00e363775e8b80b49b0a558f4df761ab1
Levy <levymcruz@gmail.com>
2023-07-25 20:18:50 -0300

    instanciando o ORM apenas uma vez no controller games

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -1,8 +1,8 @@
 import GamesORM from "../database/orm.games.js";
 
-export async function getGames(req, res){
-    const orm = new GamesORM()
+const orm = new GamesORM()
 
+export async function getGames(req, res){
     const itemById = await orm.read();
     console.log(itemById);
 
@@ -11,7 +11,6 @@ export async function getGames(req, res){
 
 export async function postGames(req, res){
     const data = req.body
-    const orm = new GamesORM()
     const item = await orm.create(data)
     console.log(item)
     res.send(item)
