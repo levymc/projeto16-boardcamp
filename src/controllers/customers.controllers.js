3f27df1d29f86c71aa43e40ba3086d6fecfcca3f
Levy <levymcruz@gmail.com>
2023-07-26 15:33:12 -0300

    fix: updating date to format YYYY-MM-DD

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -1,4 +1,5 @@
 import CustomerORM from "../database/orm.customers.js"
+import { format } from 'date-fns';
 
 const orm = new CustomerORM()
 
@@ -8,13 +9,19 @@ export async function getCustomers(req, res){
     res.send(customers)
 }
 
-export async function getCustomerById(req, res){
-    const id = req.params
-    const customer = await orm.readById(id)
-    console.log(customer)
+export async function getCustomerById(req, res) {
+    const id = req.params;
+    const customer = await orm.readById(id);
+    console.log(customer);
 
-    customer ? res.send(customer) : res.status(404).send("Usuário não encontrado!")
+    if (customer) {
+        customer.birthday = format(new Date(customer.birthday), 'yyyy-MM-dd');
+        res.send(customer).status(201);
+    } else {
+        res.status(404).send("Usuário não encontrado!");
+    }
 }
+
 
 export async function postCustomer(req, res){
     const data = req.body
