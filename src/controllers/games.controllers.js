832c57a08b212c892bd8cdabf90a6b49cea8749f
Levy <levymcruz@gmail.com>
2023-07-30 12:39:37 -0300

    adding some try catch

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -3,22 +3,35 @@ import GamesDAO from "../database/dao/dao.games.js";
 const dao = new GamesDAO()
 
 export async function getGames(req, res){
-    const itens = await dao.read();
-    // console.log(itemById);
-
-    res.send(itens)
+    try{
+        const itens = await dao.read()
+        res.send(itens)
+    }catch (err) {
+        console.error("Erro get games:", err);
+        res.status(500).send("Erro games.");
+    }
+   
 }
 
 export async function postGames(req, res){
-    const data = req.body
+    try{
+        const data = req.body
 
-    const checkName = await dao.readByName(req.body.name)
+        const checkName = await dao.readByName(req.body.name)
+    
+        console.log("AQUI ",checkName)
+    
+        if (checkName.length != 0) return res.status(409).send("O nome inserido já existe!")
+    
+        const item = await dao.create(data)
+        console.log(item)
+        res.status(201).send(item)
+    }catch (err) {
+        console.error("Erro post games:", err);
+        res.status(500).send("Erro games.");
+    }
+}
 
-    console.log("AQUI ",checkName)
-
-    if (checkName.length != 0) return res.status(409).send("O nome inserido já existe!")
-
-    const item = await dao.create(data)
-    console.log(item)
-    res.status(201).send(item)
+export async function getGameByName(req, res){
+    
 }
